18b1b6d659a83eec1529be5e1239fae0282f5a53
PaulOnyirimba <Onyirimbap@gmail.com>
2021-06-19 15:57:21 -0400

    first commit

--- E-commerce/routes/inventoryRouter.js
+++ E-commerce/routes/inventoryRouter.js
@@ -21,6 +21,47 @@ inventoryRouter.get("/", (req, res, next) => {
     return res.status(200).send(inventory)
   })
 })
+
+// Post request
+inventoryRouter.post("/", (req, res, next) => {
+    const newInventory = new Inventory(req.body)
+    newInventory.save((err, savedInventory) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(savedInventory)
+    })
+  })
+
+//   Delete request
+inventoryRouter.delete("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndDelete(
+      {_id: req.params.inventoryId}, 
+      (err, deletedItem) => {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(200).send(`Successfully deleted item ${deletedItem.title} from the database`)
+      }
+    )
+  })
+//   Put request
+inventoryRouter.put("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndUpdate(
+      { _id: req.params.inventoryID},
+      req.body,
+      {new: true},
+      (err, updatedInventory) => {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(201).send(updatedInventory)
+      }
+    )  
+  })
 // inventoryRouter.get('/', (req, res) => {
 //     res.send(inventory)
 // });
